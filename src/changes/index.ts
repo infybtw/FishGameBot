418502009b817b40c2c630300fcd0d681d129e1a
infy <user.mg1216@gmail.com>
2026-09-13 00:28:09 +0300

    feat: update changelog

--- src/changes/index.ts
+++ src/changes/index.ts
@@ -1,7 +1,8 @@
+import { change as v080 } from "./v0.8.0.ts";
 import { change as v070 } from "./v0.7.0.ts";
 import { change as v060 } from "./v0.6.0.ts";
 import { change as v055 } from "./v0.5.5.ts";
 import { change as v054 } from "./v0.5.4.ts";
 import { change as v053 } from "./v0.5.3.ts";
 
-export const LATEST_CHANGES = [v070, v060, v055, v054, v053] as const;
+export const LATEST_CHANGES = [v080, v070, v060, v055, v054, v053] as const;
